fd146802b31e074e4701f1f604e33a94981be671
Claudia Calzadillas <cdat2011@hotmail.com>
2016-01-11 09:33:55 -0500

    Uploaded website files

---
+++ public/index.js
@@ -0,0 +1,17 @@
+$(document).on("ready", function(){
+  $("#scroll-to-passion").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#passions-container").offset().top
+    }, 1000);
+  });
+  $("#scroll-to-proyects").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projects-container").offset().top
+    }, 1000);
+  });
+  $("#scroll-to-contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact-me-container").offset().top
+    }, 1000);
+  });
+})
